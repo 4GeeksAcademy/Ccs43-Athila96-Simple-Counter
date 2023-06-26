236aa55f537403f76a4498d1827f731eb72f069c
Samuel Herrera <futbol.ingeniero@gmail.com>
2023-06-26 15:09:34 +0000

    Estructura de un contador de segundos de la pagina renderizada

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,57 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "react";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+/* import { checkPropTypes } from "prop-types";
+ */
+/* import "./icons.js"; */
 
-//import your own components
-import Home from "./component/home.jsx";
+function SecondsCounter(props) {
+  return (
+    <div className="bigCounter">
+      <div className="calender">
+        <i class="far fa-clock"></i>
+      </div>
+      <div className="six">{props.digitSix % 10}</div>
+      <div className="five">{props.digitFive % 10}</div>
+      <div className="four">{props.digitFour % 10}</div>
+      <div className="three">{props.digitThree % 10}</div>
+      <div className="two">{props.digitTwo % 10}</div>
+      <div className="one">{props.digitOne % 10}</div>
+    </div>
+  );
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+/* SimpleCounter.prototype = {
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number,
+}; */
+
+let counter = 0;
+setInterval(function () {
+  const six = Math.floor(counter / 100000);
+  const five = Math.floor(counter / 10000);
+  const four = Math.floor(counter / 1000);
+  const three = Math.floor(counter / 100);
+  const two = Math.floor(counter / 10);
+  const one = Math.floor(counter / 1);
+  counter++;
+
+  //render your react application
+  ReactDOM.render(
+    <SecondsCounter
+      digitOne={one}
+      digitTwo={two}
+      digitThree={three}
+      digitFour={four}
+      digitFive={five}
+      digitSix={six}
+    />,
+    document.querySelector("#app")
+  );
+}, 1000);
